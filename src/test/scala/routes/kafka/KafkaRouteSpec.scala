ef0fad412f72b24391444a85ba7bb220cdfc036f
vgmartinez <viktor.manuel.garcia@gmail.com>
2016-10-24 18:16:40 +0200

    cambiando nombre de paquete en los test

---
+++ src/test/scala/routes/kafka/KafkaRouteSpec.scala
@@ -0,0 +1,12 @@
+package routes.kafka
+
+import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.model.headers.RawHeader
+import org.scalatest.concurrent.ScalaFutures
+import services.BaseSpec
+
+class KafkaRouteSpec extends BaseSpec with ScalaFutures{
+  "Kafka service" should {
+
+  }
+}
