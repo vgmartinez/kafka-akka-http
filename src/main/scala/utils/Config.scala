dc1d0bf5e1b09c7cadfbe85d96e91449bfb986fc
vgmartinez <viktor.manuel.garcia@gmail.com>
2016-11-24 18:07:57 +0100

    get vars from consul

--- src/main/scala/utils/Config.scala
+++ src/main/scala/utils/Config.scala
@@ -1,27 +1,19 @@
 package utils
 
-import com.typesafe.config.ConfigFactory
-
 trait Config {
-  private val config = ConfigFactory.load()
-  private val httpConfig = config.getConfig("http")
-  private val databaseConfig = config.getConfig("database")
-  private val kafkaConfig = config.getConfig("kafka")
-  private val rangerConfig = config.getConfig("ranger")
+  val httpInterface = System.getenv("COGNOS_HTTP_INTERFACE")
+  val httpPort = System.getenv("COGNOS_HTTP_PORT").toInt
 
-  val httpInterface = httpConfig.getString("interface")
-  val httpPort = httpConfig.getInt("port")
+  val databaseUrl = System.getenv("COGNOS_DATABASE_URL")
+  val databaseUser = System.getenv("COGNOS_DATABASE_USER")
+  val databasePassword = System.getenv("COGNOS_DATABASE_PASSWORD")
 
-  val databaseUrl = databaseConfig.getString("url")
-  val databaseUser = databaseConfig.getString("user")
-  val databasePassword = databaseConfig.getString("password")
+  val kafkaHost = System.getenv("COGNOS_KAFKA_HOST")
+  val kafkaPort = System.getenv("COGNOS_KAFKA_PORT").toInt
 
-  val kafkaHost = kafkaConfig.getString("default_host")
-  val kafkaPort = kafkaConfig.getInt("default_port")
-
-  val rangerHost = rangerConfig.getString("default_host")
-  val rangerPort = rangerConfig.getInt("default_port")
-  val rangerUser = rangerConfig.getString("default_user")
-  val rangerPass = rangerConfig.getString("default_pass")
+  val rangerHost = System.getenv("COGNOS_RANGER_HOST")
+  val rangerPort = System.getenv("COGNOS_RANGER_PORT").toInt
+  val rangerUser = System.getenv("COGNOS_RANGER_USER")
+  val rangerPass = System.getenv("COGNOS_RANGER_PASSWORD")
 
 }
